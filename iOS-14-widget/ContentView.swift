13d8d78748623d35d5303340d2ae2c44ec82f5f6
Leonardo Kaminski Ferreira <leonardo.kaminski.ferreira@gmail.com>
2020-06-26 01:39:25 -0300

    [FEAT] Add base project

---
+++ iOS-14-widget/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  iOS-14-widget
+//
+//  Created by Leonardo Vinicius Kaminski Ferreira on 26/06/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS-14-widget/iOS_14_widgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_14_widgetApp.swift
+//  iOS-14-widget
+//
+//  Created by Leonardo Vinicius Kaminski Ferreira on 26/06/20.
+//
+
+import SwiftUI
+
+@main
+struct iOS_14_widgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
